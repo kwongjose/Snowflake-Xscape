b00d8a5729dfcbac42ad7f30afcdb803dee1a810
Andrew Bros <andrewbros@gmail.com>
2022-03-23 20:34:21 -0500

    SQL to create curated schema and clean employee table

---
+++ transform.sql
@@ -0,0 +1,23 @@
+// just in case
+USE DATABASE escape_velocity_sales;
+
+CREATE SCHEMA IF NOT EXISTS curated
+       DATA_RETENTION_TIME_IN_DAYS = 0;
+
+USE SCHEMA curated;
+
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE IF NOT EXISTS employees (
+       EmployeeID int not null,
+       FirstName varchar not null,
+       MiddleInitial varchar null,
+       LastName varchar not null,
+       Region varchar null);
+
+// copy data from raw to curated, transforming as necessary
+INSERT INTO curated.employees (
+       EmployeeID, FirstName, MiddleInitial, LastName, Region)
+       SELECT EmployeeID, FirstName, MiddleInitial, LastName, INITCAP(Region)
+       FROM raw.employees;
+
