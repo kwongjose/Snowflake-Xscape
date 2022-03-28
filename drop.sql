fa1ec7568b3175eb51a17844eccf41ba05090357
Joseph Kwong <scq198@hotmail.com>
2022-03-28 18:57:17 -0500

    clean up drop.sql

--- drop.sql
+++ drop.sql
@@ -1,39 +1,19 @@
 USE DATABASE escape_velocity_sales;
 
 // drop stages
-DROP STAGE stage_employee;
-
-// drop tables
-DROP TABLE employees;
-
-// drop schema
-DROP SCHEMA raw;
-
-// drop stages
 DROP STAGE @stage_sales;
+DROP STAGE stage_employee;
+DROP STAGE state_products;
+DROP STAGE stage_customers;
 
 // drop tables
 DROP TABLE sales;
+DROP TABLE products;
+DROP TABLE customers;
+DROP TABLE employees;
 
 // drop schema
 DROP SCHEMA raw;
 
 // drop database
 DROP DATABASE escape_velocity_sales;
-
-// drop stages
-DROP STAGE stage_employee;
-DROP STAGE state_products;
-// drop tables
-DROP TABLE products;
-// drop schema
-DROP SCHEMA raw;
-
-
-// drop stages
-DROP STAGE stage_customers;
-DROP STAGE state_customers;
-// drop tables
-DROP TABLE customers;
-// drop schema
-DROP SCHEMA raw;
