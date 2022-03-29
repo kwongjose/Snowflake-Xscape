582475274f001e866c065edafae62b7a65decc1c
Joseph Kwong <scq198@hotmail.com>
2022-03-28 20:52:53 -0500

    named columns

--- product_sales_view.sql
+++ product_sales_view.sql
@@ -3,7 +3,7 @@ user escape_velocity_sales.curated;
 DROP VIEW IF EXISTS product_sales_view;
 
 CREATE VIEW product_sales_view AS
-	SELECT s.OrderID, s.SalesPersonID, s.CustomerID, s.ProductID, Name, Price, s.Quantity, Amount, s.Date, YEAR(s.Date) as Year, MONTH(s.Date) as Month
+	SELECT s.OrderID, s.SalesPersonID as "SalesPerson ID", s.CustomerID as "Customer ID", s.ProductID as "Product ID", Name as "Product Name", Price as "Product Price", s.Quantity, Amount as "Total Sales Amount", s.Date as "Order Date", YEAR(s.Date) as "Sales Year", MONTH(s.Date) as "Sales Month"
     FROM sales as s, LATERAL(
         SELECT
             Name,
